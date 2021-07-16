eed2b18da23744f24e9261a0e135c59e3f7d5fcb
unknown <ricardomazamiranda@gmail.com>
2021-07-15 21:34:42 -0500

    Datos subidos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,26 @@
-import 'package:proyecto1/src/usuarios.dart';
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:proyecto1/providers/ui_provider.dart';
 
-void main() => runApp(Usuarios());
+import 'pages/home_tab.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => new UiProvider())],
+      child: MaterialApp(
+        title: 'Provider',
+        initialRoute: 'home',
+        routes: {
+          'home': (_) => HomeTab(),
+        },
+        theme: ThemeData.dark(),
+      ),
+    );
+  }
+}
